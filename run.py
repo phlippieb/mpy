90fa3827824db076858f0efd39ab72db883c57eb
phlippieb <phlippie.bosman@gmail.com>
2016-11-14 06:46:07 +0200

    Fixed example run.py

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import benchmarks.spherical as spherical
-import pso.gbest_pso2 as gbest
+import psodroc.benchmarks.spherical as spherical
+import psodroc.pso.gbest_pso as gbest
 
 # run gbest pso to solve the sphere benchmark function
 
 gbest.function = spherical.spherical
 gbest.lower_bound = spherical.lower_bound
 gbest.upper_bound = spherical.upper_bound
-gbest.num_dimensions = 2
+gbest.num_dimensions = 5
 gbest.init_pso_defaults()
 gbest.init_swarm(size=25)
 
-iterations = 75
+iterations = 100
 all_fits = np.zeros([iterations, gbest.swarm_size])
 for i in range(0, iterations):
+    print 'iteration {}...'.format(i)
     gbest.iterate()
     for index, fitness in np.ndenumerate(gbest.fitnesses):
         all_fits[i, index] = fitness
