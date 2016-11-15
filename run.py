75c9945e8ea1e574bb00648dd821b0ecb9efbe1a
phlippieb <phlippie.bosman@gmail.com>
2016-11-15 06:42:54 +0200

    Example runner writes solutions to file

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 import psodroc.benchmarks.spherical as spherical
 import psodroc.pso.gbest_pso as gbest
 
-# run gbest pso to solve the sphere benchmark function
+# set up gbest to solve the spherical benchmark function
 
 gbest.function = spherical.spherical
 gbest.lower_bound = spherical.lower_bound
@@ -11,13 +12,33 @@ gbest.upper_bound = spherical.upper_bound
 gbest.num_dimensions = 5
 gbest.init_pso_defaults()
 gbest.init_swarm(size=25)
-
 iterations = 100
+
+# example 1: plotting all fitnesses over time
+
 all_fits = np.zeros([iterations, gbest.swarm_size])
 for i in range(0, iterations):
-    print 'iteration {}...'.format(i)
     gbest.iterate()
     for index, fitness in np.ndenumerate(gbest.fitnesses):
         all_fits[i, index] = fitness
 plt.plot(all_fits)
 plt.show()
+
+# example 2: writing best solutions to file
+gbest.init_swarm(size=25)
+solutions = []
+for sample in range(0, 30):
+    for iteration in range(0, iterations):
+        gbest.iterate()
+    solution = min(gbest.fitnesses)
+    solutions.append(solution)
+
+path = './results/fit/gbest.{}.spherical.{}'.format(gbest.swarm_size, gbest.num_dimensions)
+dir = os.path.dirname(path)
+if not os.path.exists(dir):
+    os.makedirs(dir)
+with open(path, 'w') as f:
+    for solution in solutions:
+        str_fitness = '{}\n'.format(solution)
+        f.write(str_fitness)
+f.closed
