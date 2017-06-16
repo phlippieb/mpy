7352135865d878c94f88af54eb3dba55d972fed8
phlippieb <phlippie.bosman@gmail.com>
2017-06-16 17:25:22 +0200

    Add quadric benchmark

---
+++ psodroc/benchmarks/quadric.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# X. Yao, Y. Liu, and G. Lin. Evolutionary Programming Made Faster. IEEE Transactions on Evolutionary Computation, 3(2):82-102, July 1999.
+
+def function(xs):
+    return np.sum(np.square(np.sum(xs)))
+
+# domain = [-100, 100] across all dimensions
+def min(d):
+    return -100
+
+def max(d):
+    return 100
+
+# min = [0.0, ..., 0.0] = 0.0
