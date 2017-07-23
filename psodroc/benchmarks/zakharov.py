54dd6dc65613a76178ac0241ea8def5cec457899
phlippieb <phlippie.bosman@gmail.com>
2017-07-23 12:08:49 +0200

    Add zakharov benchmark

---
+++ psodroc/benchmarks/zakharov.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# S. K. Mishra. Performance of Repulsive Particle Swarm Method in Global Optimization of Some Important Test Functions: A Fortran Program. Technical report, Social Science Research Network (SSRN), August 2006.
+
+def function(xs):
+    return np.sum([np.square(x) for x in xs]) \
+    + np.square(np.sum([((i+1)*x)/2 for i, x in enumerate(xs)])) \
+    + np.power(np.sum([((i+1)*x)/2 for i, x in enumerate(xs)]), 4)
+
+# domain = [-5, 10] across all dimensions
+def min(d):
+    return -5
+
+def max(d):
+    return 10
+
+# min = [0, ..., 0] = 0
