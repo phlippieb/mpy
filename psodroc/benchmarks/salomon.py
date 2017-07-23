a9f94e4ed86a0692736a478b3f58535a31c6e0a1
phlippieb <phlippie.bosman@gmail.com>
2017-07-23 16:27:37 +0200

    Fix copy paste error in salomon benchmark

--- psodroc/benchmarks/salomon.py
+++ psodroc/benchmarks/salomon.py
@@ -3,7 +3,6 @@ import numpy as np
 # K. V. Price, R. M. Storn, and J. A. Lampinen. Appendix A.1: Unconstrained Uni-Modal Test Functions. In Differential Evolution A Practical Approach to Global Optimization, Natural Computing Series, pages 514-533. Springer-Verlag, Berlin, Germany, 2005.
 
 def function(xs):
-    def function(xs):
     return -np.cos(2 * np.pi * np.sum([np.square(x) for x in xs])) \
     + 0.1 * np.sqrt(np.sum([np.square(x) for x in xs])) \
     + 1
