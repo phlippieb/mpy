8c649f59eba3d9143f44a726be002e92541ed88f
phlippieb <phlippie.bosman@gmail.com>
2017-07-23 16:59:26 +0200

    Fix schwefel 2.22 (was missing an abs)

--- psodroc/benchmarks/schwefel_2_22.py
+++ psodroc/benchmarks/schwefel_2_22.py
@@ -3,13 +3,14 @@ import numpy as np
 # X. Yao, Y. Liu, and G. Lin. Evolutionary Programming Made Faster. IEEE Transactions on Evolutionary Computation, 3(2):82-102, July 1999.
 
 def function(xs):
-    return np.sum(xs) + np.prod(xs)
+    return np.sum([np.abs(x) for x in xs]) \
+    + np.prod([np.abs(x) for x in xs])
 
 # Domain is [-10, 10] across all dimensions
 def min(d):
-    return -10
+    return -10.0
 
 def max(d):
-    return 10
+    return 10.0
 
 # Minimum is at [0, ..., 0] = 0
