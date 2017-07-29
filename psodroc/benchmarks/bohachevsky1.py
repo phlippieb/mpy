3848fda916cd3f70077b2ad8aafe77214906345f
phlippieb <phlippie.bosman@gmail.com>
2017-07-29 13:39:33 +0200

    Renamed bohachevsky to bohachevsky1_generalized, added bohachevsky1 in 2 dimensions, added tests for both

---
+++ psodroc/benchmarks/bohachevsky1.py
@@ -0,0 +1,50 @@
+# Bohachevsky, Ihor O., Mark E. Johnson, and Myron L. Stein. "Generalized simulated annealing for function optimization." Technometrics 28.3 (1986): 209-217.
+
+import numpy as np
+
+def function(xs):
+    D = len(xs)
+    if D != 2:
+        raise Exception("bohachevsky1.function is only defined for 2 dimensions.")
+
+    x1 = xs[0]
+    x2 = xs[1]
+
+    return np.square(x1) \
+        + 2 * np.square(x2) \
+        - 0.3 * np.cos(3 * np.pi * x1) \
+        - 0.4 * np.cos(4 * np.pi * x2) \
+        + 0.7
+
+# Domain is [-100, 100] for x1 and x2
+def min(d):
+    return -100.
+
+
+def max(d):
+    return +100.
+
+# Minimum is at [0, 0] = 0
+
+# Tests:
+import pytest as pt
+def _test_assert_dimensions():
+    xss = [[], [1], [1,2,3], [1,2,3,4,5]]
+    for xs in xss:
+        with pt.raises(Exception):
+            function(xs)
+
+
+def _test_min():
+    m = np.array([0., 0.])
+    assert function(m) == 0.0
+
+    for i in range(100):
+        p = np.random.uniform(low=min(2), high=min(2), size=2)
+        # Check that the minimum is less than the random point, unless the random point is the minimum:
+        assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = np.array([1., 2.])
+    assert function(xs) == 9.6
+    
--- psodroc/benchmarks/bohachevsky1_generalized.py
+++ psodroc/benchmarks/bohachevsky1_generalized.py
@@ -5,7 +5,7 @@ import numpy as np
 def function(xs):
     D = len(xs)
     if D < 2:
-        raise Exception("bohachevsky.function must have 2 or more dimensions.")
+        raise Exception("bohachevsky1_generalized.function must have 2 or more dimensions.")
 
     return np.sum([ np.square(xi) + 2 * np.square(xi1) \
                  - 0.3 * np.cos(3 * np.pi * xi) \
@@ -19,5 +19,28 @@ def min(d):
 
 def max(d):
     return 15.0
-    
+
 # min = [0.0, ... 0.0] = 0.0
+
+# Tests:
+import pytest as pt
+def _test_assert_dimensions():
+    xss = [[], [1]]
+    for xs in xss:
+        with pt.raises(Exception):
+            function(xs)
+
+
+def _test_min():
+    for D in [2, 5, 10, 20, 50]:
+        m = np.full(D, 0.)
+        assert function(m) == 0.0
+
+        for i in range(100):
+            p = np.random.uniform(low=min(2), high=min(2), size=D)
+            # Check that the minimum is less than the random point, unless the random point is the minimum:
+            assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = np.array([1., 2.])
+    assert function(xs) == 9.6
