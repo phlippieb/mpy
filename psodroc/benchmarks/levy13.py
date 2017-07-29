0f58818a0c9ca98a1c0f55a73445cccbe4bf40cd
phlippieb <phlippie.bosman@gmail.com>
2017-07-29 14:45:14 +0200

    Change levy13 benchmark to 2D; add tests

--- psodroc/benchmarks/levy13.py
+++ psodroc/benchmarks/levy13.py
@@ -4,13 +4,14 @@ import numpy as np
 
 def function(xs):
     D = len(xs)
-    if D < 2:
-        raise Exception("levy.function must have 2 or more dimensions")
+    if D != 2:
+        raise Exception("levy13.function is only defined for 2")
 
-    return np.sum([np.square(np.sin(3 * np.pi * xi)) \
-                   + np.square(xi - 1) * (1 + np.square(np.sin(3 * np.pi * xi1))) \
-                   + np.square(xi1 - 1) * (1 + np.square(np.sin(2 * np.pi * xi1))) \
-                   for xi, xi1 in zip(xs[:-1], xs[1:]) ])
+    x1 = xs[0]
+    x2 = xs[1]
+    return np.square(np.sin(3 * np.pi * x1)) \
+        + np.square(x1 - 1) * (1 + np.square(np.sin(3 * np.pi * x2))) \
+        + np.square(x2 - 1) * (1 + np.square(np.sin(2 * np.pi * x2)))
 
 # domain = [-10.0, 10.0] across all dimensions
 def min(d):
@@ -19,4 +20,20 @@ def min(d):
 def max(d):
     return 10.0
 
-# min = [1.0, ... 1.0] = 0.0
+# Minimum is [1.0, ... 1.0] = 0.0
+
+# Tests:
+import pytest as pt
+
+def _test_min():
+    m = np.array([1., 1.])
+    assert function(m) == pt.approx(0.)
+
+    for i in range(100):
+        p = np.random.uniform(low=min(0), high=max(0), size=2)
+        # Check that the minimum is less than the random point, unless the random point is the minimum:
+        assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = [2, 3]
+    assert function(xs) == 5
