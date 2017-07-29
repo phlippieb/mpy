11406af7cffd9f02622bf5d947615d44877d6f40
phlippieb <phlippie.bosman@gmail.com>
2017-07-29 12:36:49 +0200

    Add test for alpine benchmark

--- psodroc/benchmarks/alpine.py
+++ psodroc/benchmarks/alpine.py
@@ -13,3 +13,21 @@ def max(x):
     return 10.0
 
 # min = [0.0, ... 0.0] = 0.0
+
+# Tests:
+
+import pytest as pt
+def _test_min():
+    for D in [1, 2, 5, 10, 20, 50]:
+        m = np.full(D, 0.0)
+        assert function(m) == 0.0
+
+        for i in range(100):
+            p = np.random.uniform(low=min(0), high=max(0), size=D)
+            m = np.full(D, 0.)
+            # Check that the minimum is less than the random point, unless the random point is the minimum:
+            assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = [1, 2]
+    assert function(xs) == 2.96006583845926
