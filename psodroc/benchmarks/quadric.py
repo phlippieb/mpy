c65cc676b2c1d1d97acaf4bc70d590c28cc2d1fe
phlippieb <phlippie.bosman@gmail.com>
2017-08-12 14:25:04 +0200

    Add test for quadric; fix quadric

--- psodroc/benchmarks/quadric.py
+++ psodroc/benchmarks/quadric.py
@@ -3,7 +3,8 @@ import numpy as np
 # X. Yao, Y. Liu, and G. Lin. Evolutionary Programming Made Faster. IEEE Transactions on Evolutionary Computation, 3(2):82-102, July 1999.
 
 def function(xs):
-    return np.sum(np.square(np.sum(xs)))
+    D = len(xs)
+    return np.sum([np.square(np.sum(xs[j] for j in range(i+1))) for i in range(D)])
 
 # domain = [-100, 100] across all dimensions
 def min(d):
@@ -16,7 +17,7 @@ def max(d):
 
 # Tests:
 
-def _test_min_1():
+def _test_min():
     for D in [1, 2, 3, 5, 10, 20, 30, 50, 100]:
         m = np.full(D, 0.)
         assert function(m) == 0.
@@ -26,3 +27,7 @@ def _test_min_1():
             m = np.full(D, 0.)
             # Check that the minimum is less than the random point, unless the random point is the minimum:
             assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = [1., 2., 3.]
+    assert function(xs) == 46.
