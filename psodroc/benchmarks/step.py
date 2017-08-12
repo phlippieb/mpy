4d4d7cd33b441db814c8a96c229a2e24c2d73252
phlippieb <phlippie.bosman@gmail.com>
2017-08-12 16:18:03 +0200

    Add tests for step benchmark function

--- psodroc/benchmarks/step.py
+++ psodroc/benchmarks/step.py
@@ -14,3 +14,20 @@ def max(d):
 
 # min is [0, ..., 0] = 0
 # though, seemingly, it is really [(-0.5, 0.5), ..., (-0.5, 0.5)]?
+
+# Tests:
+import pytest as pt
+
+def _test_min():
+    for D in [2, 5, 10, 20, 50]:
+        m = np.full(D, 0.)
+        assert function(m) == pt.approx(0.0)
+
+        for i in range(100):
+            p = np.random.uniform(low=min(0), high=max(0), size=D)
+            # Check that the minimum is less than the random point, unless the random point is the minimum:
+            assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = [1., 2.]
+    assert function(xs) == 5.
