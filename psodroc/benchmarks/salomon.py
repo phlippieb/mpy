9169f2e1e99088022d6b41846a020ee47e613dfe
phlippieb <phlippie.bosman@gmail.com>
2017-08-12 15:30:28 +0200

    Refactor salomon benchmark

--- psodroc/benchmarks/salomon.py
+++ psodroc/benchmarks/salomon.py
@@ -3,9 +3,8 @@ import numpy as np
 # K. V. Price, R. M. Storn, and J. A. Lampinen. Appendix A.1: Unconstrained Uni-Modal Test Functions. In Differential Evolution A Practical Approach to Global Optimization, Natural Computing Series, pages 514-533. Springer-Verlag, Berlin, Germany, 2005.
 
 def function(xs):
-    return -np.cos(2 * np.pi * np.sqrt(np.sum([np.square(x) for x in xs]))) \
-    + 0.1 * np.sqrt(np.sum([np.square(x) for x in xs])) \
-    + 1
+    s = np.sqrt(np.sum([np.square(x) for x in xs]))
+    return -np.cos(2 * np.pi * s) + (0.1 * s) + 1
 
 # Domain is [-100, 100] across all dimensions
 def min(d):
