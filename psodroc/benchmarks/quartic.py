e1ffc00e7ef0d3beca36523e12d5e76523c2b071
phlippieb <phlippie.bosman@gmail.com>
2017-08-12 14:37:49 +0200

    Add tests for quartic benchmark; fix quartic benchmark

--- psodroc/benchmarks/quartic.py
+++ psodroc/benchmarks/quartic.py
@@ -3,13 +3,29 @@ import numpy as np
 # X. Yao, Y. Liu, and G. Lin. Evolutionary Programming Made Faster. IEEE Transactions on Evolutionary Computation, 3(2):82-102, July 1999.
 
 def function(xs):
-    return sum([i * np.power(x, 4) for i, x in enumerate(xs)])
+    return sum([(i+1) * np.power(x, 4) for i, x in enumerate(xs)])
 
 # domain = [-100, 100] across all dimensions
 def min(d):
-    return -100.0
+    return -1.28
 
 def max(d):
-    return 100.0
+    return 1.28
 
 # min = [0.0, ..., 0.0] = 0.0
+
+# Tests:
+
+def _test_min():
+    for D in [1, 2, 3, 5, 10, 20, 30, 50, 100]:
+        m = np.full(D, 0.)
+        assert function(m) == 0.
+
+        for i in range(100):
+            p = np.random.uniform(low=min(0), high=max(0), size=D)
+            # Check that the minimum is less than the random point, unless the random point is the minimum:
+            assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = [2., 2., 2.]
+    assert function(xs) == 96.
