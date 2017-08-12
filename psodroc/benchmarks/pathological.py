69bef024d72ff27f3012b3aed87d9209efd7d78f
phlippieb <phlippie.bosman@gmail.com>
2017-08-12 14:05:03 +0200

    Add tests for pathological benchmark

--- psodroc/benchmarks/pathological.py
+++ psodroc/benchmarks/pathological.py
@@ -22,3 +22,15 @@ def max(d):
     return 100.0
 
 # min = [0, 0, ...] = 0
+
+# Tests:
+import pytest as pt
+
+def _test_min():
+    m = np.array([0., 0.])
+    assert function(m) == 0.
+
+    for i in range(100):
+        p = np.random.uniform(low=min(0), high=max(0), size=2)
+        # Check that the minimum is less than the random point, unless the random point is the minimum:
+        assert p.all() == m.all() or function(m) < function(p)
