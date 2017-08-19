40723e7b91d158e99cde37ffe8a78a38a021d45e
phlippieb <phlippie.bosman@gmail.com>
2017-08-19 12:59:29 +0200

    Update weierstrass benchmark to have variable kMax; add tests for weierstrass benchmark

--- psodroc/benchmarks/weierstrass.py
+++ psodroc/benchmarks/weierstrass.py
@@ -2,13 +2,13 @@ import numpy as np
 
 # S. K. Mishra. Performance of Repulsive Particle Swarm Method in Global Optimization of Some Important Test Functions: A Fortran Program. Technical report, Social Science Research Network (SSRN), August 2006.
 
-ks = range(21) # i.e. [0, ..., 20]
+def function(xs, kMax=20):
+    ks = range(kMax+1) # Range is exclusive; function definition is inclusive
 
-def function(xs):
     return np.sum([np.sum([np.power(0.5, k) * np.cos(2 * np.pi * np.power(3, k) * (x + 0.5)) for k in ks]) for x in xs]) \
-    - (len(xs) * _constant())
+    - (len(xs) * _constant(ks))
 
-def _constant():
+def _constant(ks):
     return np.sum([np.power(0.5, k) * np.cos(2 * np.pi * np.power(3, k) * 0.5) for k in ks])
 
 # domain is [-0.5, -0.5] in all dimensions
@@ -19,3 +19,20 @@ def max(d):
     return 0.5
 
 # min = [0, ..., 0] = 0
+
+# Tests:
+import pytest as pt
+
+def _test_min():
+    for D in [2, 5, 10, 20, 50]:
+        m = np.full(D, 0.)
+        assert function(m) == pt.approx(0.0)
+
+        for i in range(100):
+            p = np.random.uniform(low=min(0), high=max(0), size=D)
+            # Check that the minimum is less than the random point, unless the random point is the minimum:
+            assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = [.1, .1]
+    assert function(xs, kMax=2) == pt.approx(1.786474508)
