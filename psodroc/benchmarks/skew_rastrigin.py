a463b58b5b4749f994adceb4bc9c7aede820bc6d
phlippieb <phlippie.bosman@gmail.com>
2017-08-19 13:13:27 +0200

    Add tests for skew rastrigin benchmark function

--- psodroc/benchmarks/skew_rastrigin.py
+++ psodroc/benchmarks/skew_rastrigin.py
@@ -17,3 +17,22 @@ def max(d):
     return 5.0
 
 # Minimum is [0, ..., 0] = 0
+
+# Tests:
+import pytest as pt
+
+def _test_min():
+    for D in [2, 5, 10, 20, 50]:
+        m = np.full(D, 0.)
+        assert function(m) == pt.approx(0.0)
+
+        for i in range(100):
+            p = np.random.uniform(low=min(0), high=max(0), size=D)
+            # Check that the minimum is less than the random point, unless the random point is the minimum:
+            assert p.all() == m.all() or function(m) < function(p)
+
+def _test_other():
+    xs = [1., 2.]
+    assert function(xs) == 500
+    xs = [-1., -2.]
+    assert function(xs) == 5
