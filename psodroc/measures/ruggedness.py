2ce726781c2ce3100185f6b5123f12275f8a008f
phlippieb <phlippie.bosman@gmail.com>
2018-02-10 13:41:32 +0200

    Update ruggedness measures

--- psodroc/measures/ruggedness.py
+++ psodroc/measures/ruggedness.py
@@ -1,5 +1,7 @@
+from __future__ import division # make division cast to double by default
 import math
 import progressive_random_walk as walk
+import itertools
 
 # This file contains functions that estimate the ruggedness of a fitness landscape using first entropic measures.
 # It contains two such functions: FEM_0.1, which is used to measure the macro-ruggedness of the landscape, and FEM_0.01, which measures the micro-ruggedness.
@@ -13,24 +15,24 @@ import progressive_random_walk as walk
 # Given a fitness function, this function returns a value in [0, 1] indicating the macro ruggedness of the given function. A result of 0 indicates that the given function is completely flat. A result of 1 indicates that the given function is maximally rugged.
 
 
-def FEM_0_1(function, dimensions):
-    return _FEM(function, dimensions, 0.1)
+def FEM_0_1(function, domain_min, domain_max, dimensions):
+    return _FEM(function, domain_min, domain_max, dimensions, 0.1)
 
-def FEM_0_01(function, dimensions):
-    return _FEM(function, dimensions, 0.01)
+def FEM_0_01(function, domain_min, domain_max, dimensions):
+    return _FEM(function, domain_min, domain_max, dimensions, 0.01)
 
-def _FEM(function, dimensions, max_step_size_fraction):
+def _FEM(function , domain_min, domain_max, dimensions, max_step_size_fraction):
     # Get fitnesses from a progressive random walk:
-    domain_min = function.min(0)
-    domain_max = function.max(0)
     max_step_size = (domain_max - domain_min) * max_step_size_fraction
-    walk_positions = walk.progressive_random_walk(domain_min, domain_max, dimensions, 1000, max_step_size)
-    walk_fitnesses = function.function(walk_positions)
+    xs = walk.progressive_random_walk(domain_min, domain_max, dimensions, 1000, max_step_size)
+    walk_fitnesses = [function(x) for x in xs]
     
     # Set the entropy estimation sensitivity to 0 to begin with:
     E = 0
     entropies = []
     
+    # Increment E by 0.05 until the resulting entropy string is flat.
+    # Calculate the entropy of the string for each E.
     while True:
         entropy_string = _entropy_string(walk_fitnesses, E)
         if _is_entropy_string_flat(entropy_string):
@@ -39,50 +41,40 @@ def _FEM(function, dimensions, max_step_size_fraction):
         entropies.append(entropy)
         E += .05
     
+    # The entropy metric is the max entropy for all values of E tried.
     return max(entropies)
     
+# Build a string of symbols indicating pair-wise changes in the given array of fitnesses.
+# A difference between a pair of fitnesses that is less than the sensitivity parameter E is represented by a 0.
+# A difference larger than E is represented by -1 or +1, depending on which fitness in the pair is larger.
 def _entropy_string(fitnesses, E):
-    # TODO: check that len(fitnesses) > 1
     string = []
     fitness_pairs = [(fitnesses[i], fitnesses[i+1]) for i in range(0, len(fitnesses) - 1)]
-    for pair in fitness_pairs:
-        if abs(pair[0] - pair[1]) <= E:
-            string.append(0)
-        
-        elif pair[0] - pair[1] < -E:
-            string.append(-1)
-            
-        elif pair[0] - pair[1] > E:
-            string.append(1)
-    
+    for f0, f1 in fitness_pairs:
+        if abs(f0 - f1) <= E:   string.append(0)
+        elif f0 - f1 < -E:      string.append(-1)
+        elif f0 - f1 > E:       string.append(1)
     return string
             
-            
-    
 def _entropy(entropy_string):
-    sum = 0
-    for p in [-1, 0, 1]:
-        for q in [-1, 0, 1]:
-            if p != q:
-                P = _probability(p, q, entropy_string)
-                if P != 0:
-                    sum += (P * math.log(P, 6))
-    return -sum
+    # Compute the probability of the substring [p, q] in entropy_string for each p and q in [-1, 0, 1] such that p != q.
+    # (This gives the probability of each non-flat symbol.)
+    Ps = [_probability(p, q, entropy_string) for p, q in itertools.permutations([-1, 0, 1], 2)]
+    # Return the entropy based on those probabilities:
+    # TODO: the 0 if P == 0 part is needed because log breaks otherwise, but it is not specified in the log formula. Check it out.
+    return -sum(0 if P == 0 else (P * math.log(P, 6)) for P in Ps)
+    
 
+# Returns the number of occurences of pq in the given string, divided by the length of the string.
 def _probability(p, q, entropy_string):
-    occurences = 0
-    for i in range(0, len(entropy_string) - 1):
-        if entropy_string[i] == p and entropy_string[i+1] == q:
-            occurences += 1
+    # Count the number of times p and q occur consecutively in the string:
+    occurences = sum(1 if p_ == p and q_ == q else 0 for p_, q_ in zip(entropy_string[:-1], entropy_string[1:]))
+    # The probability of pq is the number of occurences out of the length of the string.
     return occurences / len(entropy_string)
     
+# True if and only if the string contains only 0s, indicating no significant differences in fitnesses in the walk.
 def _is_entropy_string_flat(entropy_string):
     if 1 in entropy_string or -1 in entropy_string:
         return False
     else:
         return True
-    # return entropy_string.contains(1) or entropy_string.contains(-1)
-    # for symbol in entropy_string:
-    #     if symbol != 0:
-    #         return False
-    # return True    
--- run.py
+++ run.py
@@ -1,60 +1,7 @@
 import matplotlib.pyplot as plt
 import psodroc.benchmarks.rastrigin as f
 import psodroc.pso.gc_von_neumann_pso as pso
-import psodroc.measures.diversity as diversity
-import psodroc.measures.two_piecewise_linear_approximation as twpla
+import psodroc.measures.ruggedness as ruggedness
 
-# pso.function = f.function
-# pso.lower_bound = f.min(0)
-# pso.upper_bound = f.max(0)
-# pso.num_dimensions = 40
-# pso.init_pso_defaults()
-# pso.init_swarm(size=50)
-# 
-# iterations = 1000
-# 
-# # How often (in terms of iterations) to take diversity measurements:
-# div_interval = 1
-# # Iterations at which diversity measures are taken
-# div_xs = []
-# # Diversity measurements taken
-# div_ys = []
-# 
-# # gbest fitnesses at each iteration:
-# gbests = []
-# 
-# for i in range(0, iterations):
-#     pso.iterate()
-# 
-#     # Capture diversity
-#     if i % div_interval == 0:
-#         div = diversity.avg_distance_around_swarm_centre(pso.positions)
-#         div_xs.append(i)
-#         div_ys.append(div)
-# 
-#     # Capture gbest
-#     gbests.append(pso.gbest_fitness)
-# 
-# # Calculate a two-piecewise linear approximation of all diversity measurements
-# droc = twpla.fit_to(div_xs, div_ys)
-# 
-# # Plot all diversity measurements as points, and the two-piecewise linear approximation of the diversity measurements as lines
-# plt.plot(div_xs, div_ys, ".", color="grey")
-# twpla.plot(len(div_xs)-1, droc)
-# plt.show()
-# 
-# plt.plot(gbests)
-# plt.show()
-
-# print(gbests)
-
-import psodroc.measures.funnels as funnels
-import numpy as np  
-
-all_disps = []
-for i in range(0, 10):
-    disp = funnels.dispersion_metric(f.function, f.min(0), f.max(0), 2)
-    all_disps.append(disp)
-    print("disp {}: {}".format(i, disp))
-mean_disp = np.average(all_disps)
-print("\nmean: {}".format(mean_disp))
+r = ruggedness.FEM_0_01(f.function, f.min(0), f.max(0), 1)
+print(r)
