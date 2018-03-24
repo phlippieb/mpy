f401277e24ce9dbc1cd19a492eb259c71ca28fc1
phlippieb <phlippie.bosman@gmail.com>
2018-03-24 12:20:00 +0200

    Fix run.py

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ parser = OptionParser()
 parser.add_option('--block', dest='block_number')
 parser.add_option('--of', dest='total_blocks')
 options, args = parser.parse_args()
-block_number = int(options.block_number)
-total_blocks = int(options.total_blocks)
+block_number = options.block_number
+total_blocks = options.total_blocks
 
 if block_number is not None and total_blocks is not None:
     print 'processing block', block_number, 'of', total_blocks, 'droc result blocks...'
-    drocs.process(block_number, total_blocks)
+    drocs.process(int(block_number), int(total_blocks))
     
 elif block_number is not None:
     raise Exception('When providing a block_number arg, a total_blocks arg is required.')
@@ -20,4 +20,4 @@ elif total_blocks is not None:
 
 else:
     print 'processing all droc results...'
-    # drocs.process(0, 1)
+    drocs.process(0, 1)
