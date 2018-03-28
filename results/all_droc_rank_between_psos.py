f3057a7a61dea46516f296d292a2d2a12248f2bc
phlippieb <phlippie.bosman@gmail.com>
2018-03-28 13:17:39 +0200

    Update experimental scope

--- results/all_droc_rank_between_psos.py
+++ results/all_droc_rank_between_psos.py
@@ -1,96 +1,100 @@
 import results.droc_rank_between_psos as rank
 import benchmarks
 
-pso_names = [
-    'alternative_barebones_pso', 
-    'barebones_pso', 
-    'gbest_pso', 
-    # 'gc_gbest_pso', 
-    # 'gc_lbest_pso', 
-    # 'gc_von_neumann_pso', 
-    'lbest_pso', 
-    'social_only_pso', 
-    'von_neumann_pso']
-
-swarm_sizes = [
-    2,
-    3, 
-    5,  
-    25, 
-    2000,
-    5000]
-    
-benchmark_names = [
-    # 'ackley', 
-    'alpine', 
-    # 'beale', 
-    # 'bohachevsky1_generalized', 
-    # 'eggholder', 
-    # 'goldstein_price', 
-    'griewank', 
-    # 'levy13_generalized', 
-    # 'michalewicz', 
-    # 'pathological', 
-    # 'quadric', 
-    # 'quartic', 
-    # 'rastrigin', 
-    # 'rosenbrock', 
-    # 'salomon', 
-    # 'schwefel_2_22', 
-    'schwefel_2_26', 
-    # 'six_hump_camel_back', 
-    # 'skew_rastrigin', 
-    # 'spherical', 
-    # 'step', 
-    'weierstrass']
-    # 'zakharov']
-    
-dimensionalities = [
-    5,
-    25,
-    50,
-    100]
-    
-nums_iterations = [
-    25, 
-    50, 
-    75, 
-    100,
-    125, 
-    150, 
-    2000, 
-    10000]
+_configs = []
     
 def process(batch_num, num_batches):
-    _make_configs()
+    all_pso_names = [
+        'alternative_barebones_pso', 
+        'barebones_pso', 
+        'gbest_pso', 
+        'gc_gbest_pso', 
+        'gc_lbest_pso', 
+        'gc_von_neumann_pso', 
+        'lbest_pso', 
+        'social_only_pso', 
+        'von_neumann_pso'
+    ]
+        
+    all_swarm_sizes = [
+        5, 
+        10, 
+        25, 
+        50, 
+        75, 
+        100,
+        500
+    ]
+    
+    all_benchmark_names = [
+        'spherical', # Unimodal, smooth, simple
+        'rastrigin', # Rugged, unimodal on macro-scale
+        'rosenbrock', # Smooth, multimodal in 4 dimensions and up
+        'weierstrass' # Rugged on micro- and macro-scale
+    ]
+    
+    all_dimensionalities = [
+        5,
+        25,
+        50,
+        100,
+        500,
+        1000
+    ]
+    
+    all_nums_iterations = [
+        25, 
+        50, 
+        75, 
+        100,
+        125, 
+        150, 
+        2000, 
+        10000
+    ]
+    
+    global _configs
+    
+    # Configurations for comparing DRoC performance for different swarm sizes:
+    # (with fixed 5D benchmarks and 2000 iterations per PSO)
+    _make_configs(all_pso_names, all_swarm_sizes, all_benchmark_names, [5], [2000])
+    
+    # Configurations for comparing DRoC performance at different dimensions:
+    # (with fixed 25-particle swarms and 2000 iterations)
+    _make_configs(all_pso_names, [25], all_benchmark_names, all_dimensionalities, [2000])
+    
+    # Configurations for comparing DRoC performance at different numbers of iterations:
+    # (with fixed 25-particle swarms and 5D benchmarks)
+    _make_configs(all_pso_names, [25], all_benchmark_names, [5], all_nums_iterations)
+    
+    # Process all the configs (in batches as specified):
     num_configs = len(_configs)
     batch_indices = range(batch_num, num_configs, num_batches)
     batch_size = len(batch_indices)
     
     for (i, index) in enumerate(batch_indices):
         config = _configs[index]
-        print ' -', i, 'of', batch_size
+        print 'Rank', i, 'of', batch_size
         rank.get(*config)
         
-_configs = []
 
-def _make_configs():
-    if len(_configs) > 0:
-        return
+def _make_configs(pso_names, swarm_sizes, benchmark_names, dimensionalities, nums_iterations):
+    print 'determining configurations...'
+    print '(processing', len(pso_names), 'pso names,', len(swarm_sizes), 'swarm sizes,', len(benchmark_names), 'benchmark names,', len(dimensionalities), 'dimensionalities, and', len(nums_iterations), 'numbers of iterations)'
     
-    print 'determining configurations',
-    for i in range(0, len(pso_names)):
-        for j in range(i+1, len(pso_names)):
-            print '.',
-            # All combinations are allowed except for ABBPSO and anything besides BBPSO (for now)
-            pso_1_name = pso_names[i]
-            pso_2_name = pso_names[j]
-            if pso_1_name != 'alternative_barebones_pso' or pso_2_name == 'barebones_pso':
-                for swarm_size in swarm_sizes:
-                    for benchmark_name in benchmark_names:
-                        for dimensionality in dimensionalities:
-                            if benchmarks.get(benchmark_name).is_dimensionality_valid(dimensionality):
-                                for num_iterations in nums_iterations:
-                                    config = (pso_1_name, pso_2_name, swarm_size, benchmark_name, dimensionality, num_iterations)
-                                    _configs.append(config)        
-    print '\ndone.'
+    global _configs
+    
+    for pso_1_name in pso_names:
+        for pso_2_name in pso_names:
+            if pso_1_name == pso_2_name:
+                continue
+            for swarm_size in swarm_sizes:
+                for benchmark_name in benchmark_names:
+                    benchmark = benchmarks.get(benchmark_name)
+                    for dimensionality in dimensionalities:
+                        if not benchmark.is_dimensionality_valid(dimensionality):
+                            continue
+                        for num_iterations in nums_iterations:
+                            config = (pso_1_name, pso_2_name, swarm_size, benchmark_name, dimensionality, num_iterations)
+                            _configs.append(config)
+    print 'done.'
