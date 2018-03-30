e815ca9b3896a938f24a2eef36a4235c1be907d7
phlippieb <phlippie.bosman@gmail.com>
2018-03-30 14:29:01 +0200

    Update experiment script: remove barebones algorithms and weierstrass; add prep mode

--- results/all_droc_rank_between_psos.py
+++ results/all_droc_rank_between_psos.py
@@ -1,12 +1,14 @@
 import results.droc_rank_between_psos as rank
+import results.drocs as drocs
 import benchmarks
 
 _configs = []
+_prep_configs = []
     
-def process(batch_num, num_batches):
+def process(batch_num, num_batches, prep=False):
     all_pso_names = [
-        'alternative_barebones_pso', 
-        'barebones_pso', 
+        # 'alternative_barebones_pso', 
+        # 'barebones_pso', 
         'gbest_pso', 
         'gc_gbest_pso', 
         'gc_lbest_pso', 
@@ -29,8 +31,8 @@ def process(batch_num, num_batches):
     all_benchmark_names = [
         'spherical', # Unimodal, smooth, simple
         'rastrigin', # Rugged, unimodal on macro-scale
-        'rosenbrock', # Smooth, multimodal in 4 dimensions and up
-        'weierstrass' # Rugged on micro- and macro-scale
+        'rosenbrock' # Smooth, multimodal in 4 dimensions and up
+        # 'weierstrass' # Rugged on micro- and macro-scale
     ]
     
     all_dimensionalities = [
@@ -57,25 +59,45 @@ def process(batch_num, num_batches):
     
     # Configurations for comparing DRoC performance for different swarm sizes:
     # (with fixed 5D benchmarks and 2000 iterations per PSO)
-    _make_configs(all_pso_names, all_swarm_sizes, all_benchmark_names, [5], [2000])
+    if prep:
+        _make_prep_configs(all_pso_names, all_swarm_sizes, all_benchmark_names, [5], [2000])
+    else:
+        _make_configs(all_pso_names, all_swarm_sizes, all_benchmark_names, [5], [2000])
     
     # Configurations for comparing DRoC performance at different dimensions:
     # (with fixed 25-particle swarms and 2000 iterations)
-    _make_configs(all_pso_names, [25], all_benchmark_names, all_dimensionalities, [2000])
+    if prep:
+        _make_prep_configs(all_pso_names, [25], all_benchmark_names, all_dimensionalities, [2000])
+    else:
+        _make_configs(all_pso_names, [25], all_benchmark_names, all_dimensionalities, [2000])
     
     # Configurations for comparing DRoC performance at different numbers of iterations:
     # (with fixed 25-particle swarms and 5D benchmarks)
-    _make_configs(all_pso_names, [25], all_benchmark_names, [5], all_nums_iterations)
+    if prep:
+        _make_prep_configs(all_pso_names, [25], all_benchmark_names, [5], all_nums_iterations)
+    else:
+        _make_configs(all_pso_names, [25], all_benchmark_names, [5], all_nums_iterations)
     
     # Process all the configs (in batches as specified):
-    num_configs = len(_configs)
-    batch_indices = range(batch_num, num_configs, num_batches)
-    batch_size = len(batch_indices)
+    if prep:
+        num_configs = len(_prep_configs)
+        batch_indices = range(batch_num, num_configs, num_batches)
+        batch_size = len(batch_indices)
+        
+        for (i, index) in enumerate(batch_indices):
+            prep_config = _prep_configs[index]
+            print 'DRoC', i, 'of', batch_size
+            drocs.get(*prep_config)
+        
+    else:
+        num_configs = len(_configs)
+        batch_indices = range(batch_num, num_configs, num_batches)
+        batch_size = len(batch_indices)
     
-    for (i, index) in enumerate(batch_indices):
-        config = _configs[index]
-        print 'Rank', i, 'of', batch_size
-        rank.get(*config)
+        for (i, index) in enumerate(batch_indices):
+            config = _configs[index]
+            print 'Rank', i, 'of', batch_size
+            rank.get(*config)
         
 
 def _make_configs(pso_names, swarm_sizes, benchmark_names, dimensionalities, nums_iterations):
@@ -100,3 +122,19 @@ def _make_configs(pso_names, swarm_sizes, benchmark_names, dimensionalities, num
                             config = (pso_1_name, pso_2_name, swarm_size, benchmark_name, dimensionality, num_iterations)
                             _configs.append(config)
     print 'done.'
+    
+def _make_prep_configs(pso_names, swarm_sizes, benchmark_names, dimensionalities, nums_iterations):
+    global _prep_configs
+    
+    for pso_name in pso_names:
+        for swarm_size in swarm_sizes:
+            for benchmark_name in benchmark_names:
+                for dimensionality in dimensionalities:
+                    benchmark = benchmarks.get(benchmark_name)
+                    for dimensionality in dimensionalities:
+                        if not benchmark.is_dimensionality_valid(dimensionality):
+                            continue
+                        for num_iterations in nums_iterations:
+                            for experiment_num in range(0, 30):
+                                prep_config = (pso_name, swarm_size, benchmark_name, dimensionality, num_iterations, experiment_num)
+                                _prep_configs.append(prep_config)
--- run.py
+++ run.py
@@ -5,10 +5,12 @@ parser = OptionParser()
 parser.add_option('--batch', dest='batch_number')
 parser.add_option('--of', dest='total_batches')
 # parser.add_option('--benchmark', dest="is_benchmark", action="store_true", default=False)
+parser.add_option('--prep', dest='prep', action='store_true', default=False)
 options, args = parser.parse_args()
 # is_benchmark = options.is_benchmark
 batch_number = options.batch_number
 total_batches = options.total_batches
+prep = options.prep
 
 # if is_benchmark:
 #     print 'benchmarking'
@@ -16,7 +18,7 @@ total_batches = options.total_batches
 
 if batch_number is not None and total_batches is not None:
     print 'processing block', batch_number, 'of', total_batches, 'droc result blocks...'
-    ranks.process(int(batch_number), int(total_batches))
+    ranks.process(int(batch_number), int(total_batches), prep)
 
 elif batch_number is not None:
     raise Exception('When providing a batch_number arg, a total_batches arg is required.')
@@ -26,7 +28,7 @@ elif total_batches is not None:
 
 else:
     print 'processing all droc results...'
-    ranks.process(0, 1)
+    ranks.process(0, 1, prep)
 
 # # Example of a ranked PSO-vs-PSO droc result:
 # import results.droc_rank_between_psos as droc_ranks
