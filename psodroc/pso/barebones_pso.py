d8c6e851d4f1251d898ed333aef4ef3e793fe1c5
phlippieb <phlippie.bosman@gmail.com>
2018-03-31 12:38:53 +0200

    Update barebones PSO

--- psodroc/pso/barebones_pso.py
+++ psodroc/pso/barebones_pso.py
@@ -1,122 +1,118 @@
-# Implemented as per
-# https://pdfs.semanticscholar.org/8c38/4ec0ff05ace6326e366b131ee99d20bb1c6b.pdf
-# TODO: source the original paper
-
 import numpy as np
 
 # Swarm variables
 swarm_size = 0
 positions = None
 fitnesses = None
-pbest_positions = None # each particle's personal best
+pbest_positions = None # Each particle's personal best
 pbest_fitnesses = None
-lbest_positions = None # each particle's local best
-lbest_fitnesses = None
-gbest_position = None
-gbest_fitness = None # the swarm's best fitness; not used by the algorithm, but exposed as a metric
+gbest_position = None # The swarm's personal best
+gbest_fitnesse = None
 
 # Search space
 function = None
 num_dimensions = None
-lower_bound = None # (same across all dimensions)
+lower_bound = None # Same in all dimensions
 upper_bound = None
 
-def init_swarm(size):
-    # Initializes swarm with `size` number of particles.
-    # Requires all search space parameters already to be set.
-    _validate_search_space()
 
+def init_swarm(size):
+    # Initialize a swarm with `size` particles.
+    # Requires all search space parameters to be set.
+    _validate_search_space()
+    
     global swarm_size
     swarm_size = size
-
+    
+    # The swarm positions are initialized randomly across the search space.
     global positions
     positions = np.random.rand(swarm_size, num_dimensions) * (upper_bound - lower_bound) + lower_bound
-
+    
+    # The current fitnesses are determined for each particle's position.
     global fitnesses
     fitnesses = [function(position) for position in positions]
-
+    
+    # Each particle's initial personal best position is its current position.
     global pbest_positions
     pbest_positions = np.copy(positions)
-
+    
+    # Each particle's initial personal best fitness is its current fitness.
     global pbest_fitnesses
-    pbest_fitnesses = [function(position) for position in pbest_positions]
-
-    global gbest_position
-    global gbest_fitness
+    pbest_fitnesses = np.copy(fitnesses)
+    
+    # The fittest particle is determined, and is used as the swarm's gbest.
     gbest_index = np.argmin(pbest_fitnesses)
+    
+    global gbest_position
     gbest_position = pbest_positions[gbest_index]
+    
+    global gbest_fitness
     gbest_fitness = pbest_fitnesses[gbest_index]
-
+    
 def init_pso_defaults():
-    # Nothing to init
-    return
-
+    # Nothing to init.
+    pass
+    
 
 def iterate():
     # Performs one iteration of the algorithm.
     # Afterwards, the swarm's velocities, positions, fitnesses, personal best positions,
-    # personal best fitnesses, and local best positions and fitnesses will be updated.
+    # personal best fitnesses, and global best position and fitness will be updated.
     # Requires the algorithm's and swarm's parameters to have been initialized.
     _validate_swarm()
-
-    global velocities
+    
+    # Each particle's position is sampled around a theoretical attraction point
+    # between its personal and global best.
     global positions
+    positions = [_updated_position(i) for i in range(swarm_size)]
+    
+    # Update each particle's fitness at its new position.
     global fitnesses
+    fitnesses = [function(position) for position in positions]
+    
+    # Update each particle's pbest.
     global pbest_positions
     global pbest_fitnesses
-    global gbest_position
-    global gbest_fitness
+    pbests_changed = False
+    for i in range(swarm_size):
+        if fitnesses[i] <= pbest_fitnesses[i]:
+            pbest_positions[i] = positions[i]
+            pbest_fitnesses[i] = fitnesses[i]
+            pbests_changed = True
+    
+    # Update the swarm's gbest.
+    if pbests_changed:
+        gbest_index = np.argmin(pbest_fitnesses)
+        global gbest_position
+        gbest_position = pbest_positions[gbest_index]
+        global gbest_fitness
+        gbest_fitness = pbest_fitnesses[gbest_index]
+            
+def _updated_position(i):
+    # Sample and return a new position for particle i.
+    pbest_position = pbest_positions[i]
+    
+    # The new position is sampled from a normal (Gaussian) distribution.
+    # The deviation of the distribution is determined per dimension:
+    return [_updated_position_component(pbest_position_component, gbest_position_component) \
+        for pbest_position_component, gbest_position_component in zip(pbest_position, gbest_position)]
 
-    # Update each particle's velocity using the current personal and global best positions.
-    velocities = [_velocity(pbest_position, gbest_position) for pbest_position in pbest_positions]
-
-    # Update each particle's position to its velocity.
-    positions = velocities
-
-    # Update fitnesses from positions.
-    fitnesses = [function(position) for position in positions]
-
-    # Update the personal global best positions and fitnesses.
-    new_pbest_positions = []
-    for (old_pbest_position, old_pbest_fitness, current_position, current_fitness) in zip(pbest_positions, pbest_fitnesses, positions, fitnesses):
-        if current_fitness < old_pbest_fitness:
-            new_pbest_positions.append(current_position)
-        else:
-            new_pbest_positions.append(old_pbest_position)
-
-    pbest_positions = new_pbest_positions
-    pbest_fitnesses = [function(pbest_position) for pbest_position in pbest_positions]
-
-    gbest_index = np.argmin(pbest_fitnesses)
-    gbest_position = pbest_positions[gbest_index]
-    gbest_fitness = pbest_fitnesses[gbest_index]
-
-def _velocity(pbest_position, gbest_position):
-    # In barebones PSO, the velocity of a particle is a vector sampled from a Gaussian distribution with its mean between the particle's pbest and gbest positions, and with a deviation of the distance between those two points.
-    # This returns such a vector.
-
-    # If pbest == gbest, the attractor point is simply that point.
-    if np.array_equal(pbest_position, gbest_position):
-        return pbest_position
-
-    mean = (pbest_position + gbest_position) / 2
-    deviation = np.abs(pbest_position - gbest_position)
-
-    velocity = _gaussian(mean, deviation)
-    return velocity
-
-def _gaussian(mean, deviation):
-    # Numpy's random.normal expects deviation to be > 0 in every dimension.
-    # This sidesteps that by just using the mean in any dimension where the deviation is zero.
-    result = mean
-    i = 0
-    for (mean_i, deviation_i) in zip(mean, deviation):
-        if deviation_i > 0:
-            velocity_i = np.random.normal(mean_i, deviation_i)
-            result[i] = velocity_i
-        i += 1
-    return result
-
+def _updated_position_component(pbest_position_component, gbest_position_component):
+    # Sample and return a new value for a dimensional component of a particle's position.
+    # The position component is sampled from a normal (Gaussian) distribution.
+    # The deviation of the distribution is the absolute difference between the position components.
+    # If the pbest and gbest components coincide, and the distribution deviation is zero, no sampling is required (or possible).
+    if pbest_position_component == gbest_position_component:
+        return pbest_position_component
+    else:
+        distribution_deviation = abs(pbest_position_component - gbest_position_component)
+    
+    # The mean of the distribution is the mean of the two components.
+    distribution_mean = (pbest_position_component + gbest_position_component) / 2
+    
+    # Sample and return.
+    return np.random.normal(distribution_mean, distribution_deviation)
+    
 # Validation:
 
 _did_validate_search_space = False
@@ -142,3 +138,4 @@ def _validate_swarm():
     assert gbest_position is not None, "gbest_pso.init_swarm was not called"
     assert gbest_fitness is not None, "gbest_pso.init_swarm was not called"
     _did_validate_swarm = True
+    
