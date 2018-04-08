671919c52becd043909d0235cbc4e9f217099a87
phlippieb <phlippie.bosman@gmail.com>
2018-04-08 10:48:12 +0200

    Speed up Ackley

--- psodroc/benchmarks/ackley.py
+++ psodroc/benchmarks/ackley.py
@@ -1,6 +1,7 @@
 # make division cast to double by default:
 from __future__ import division
 import numpy as np
+from numba import vectorize
 
 # X. Yao, Y. Liu, and G. Lin. Evolutionary Programming Made Faster. IEEE Transactions on Evolutionary Computation, 3(2):82-102, July 1999.
 
@@ -8,10 +9,18 @@ def function(xs):
     D = len(xs)
     assert D > 0, "ackley.function must have 1 or more dimensions."
 
-    return -20. * np.exp(-.2 * (np.sqrt(sum(np.square(x) for x in xs) / D))) \
-    - np.exp(sum(np.cos(2. * np.pi * x) for x in xs) / D) \
+    return -20. * np.exp(-.2 * (np.sqrt(np.sum(_inner1(xs)) / D))) \
+    - np.exp(sum(_inner2(xs)) / D) \
     + 20. \
     + np.exp(1)
+
+@vectorize(['float64(float64)'])
+def _inner1(x):
+    return np.square(x)
+
+@vectorize(['float64(float64)'])
+def _inner2(x):
+    return np.cos(2. * np.pi * x)
 
 # Domain is [-32.0, 32.0] across all dimensions
 def min(d):
@@ -19,7 +28,7 @@ def min(d):
 
 def max(d):
     return 32.
-    
+
 def is_dimensionality_valid(D):
     # Ackley divides by D, so D cannot be zero.
     return D > 0
