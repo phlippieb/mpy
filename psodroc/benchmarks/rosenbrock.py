ba37c3b80b7f4f6b110afcd568c1df71ddc627fc
phlippieb <phlippie.bosman@gmail.com>
2018-04-08 09:21:24 +0200

    Speed up Rosenbrock

--- psodroc/benchmarks/rosenbrock.py
+++ psodroc/benchmarks/rosenbrock.py
@@ -1,14 +1,16 @@
 import numpy as np
+from numba import vectorize
 
 # X. Yao, Y. Liu, and G. Lin. Evolutionary Programming Made Faster. IEEE Transactions on Evolutionary Computation, 3(2):82-102, July 1999.
 
 def function(xs):
     D = len(xs)
     assert D > 1, "rosenbrock.function must have 2 or more dimensions."
+    return np.sum(_inner(xs[:-1], xs[1:]))
 
-    return np.sum((100. * np.square(xi1 - np.square(xi))) + np.square(xi - 1.) \
-        for xi, xi1 in zip(xs[:-1], xs[1:]))
-
+@vectorize(['float64(float64, float64)'])
+def _inner(xi, xi1):
+    return (100. * np.square(xi1 - np.square(xi))) + np.square(xi - 1.)
 
 # Domain is [-2.048, 2.048] across all dimensions
 def min(d):
@@ -16,7 +18,7 @@ def min(d):
 
 def max(d):
     return 2.048
-    
+
 def is_dimensionality_valid(D):
     # Rosenbrock requires 2 or more dimensions.
     return D > 1
