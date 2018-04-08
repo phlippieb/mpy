206c8eeafd961bdfd1cb5dbef1d018267e10584e
phlippieb <phlippie.bosman@gmail.com>
2018-04-08 12:35:04 +0200

    Speed up Step (slightly)

--- psodroc/benchmarks/step.py
+++ psodroc/benchmarks/step.py
@@ -1,9 +1,14 @@
 import numpy as np
+from numba import vectorize
 
 # X. Yao, Y. Liu, and G. Lin. Evolutionary Programming Made Faster. IEEE Transactions on Evolutionary Computation, 3(2):82-102, July 1999.
 
 def function(xs):
-    return np.sum(np.square(np.floor([x + 0.5 for x in xs])))
+    return np.sum(_inner(xs))
+
+@vectorize(['float64(float64)'])
+def _inner(x):
+    return np.square(np.floor(x + 0.5))
 
 # domain is [-20, 20] across all dimensions
 def min(d):
@@ -11,7 +16,7 @@ def min(d):
 
 def max(d):
     return 20.
-    
+
 def is_dimensionality_valid(D):
     return True
 
