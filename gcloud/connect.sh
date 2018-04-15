237e73318ffabe75da08753cdcbdf80f7e412686
phlippieb <phlippie.bosman@gmail.com>
2018-04-15 11:27:00 +0200

    Update gcloud connect script

--- gcloud/connect.sh
+++ gcloud/connect.sh
@@ -1,5 +1,5 @@
 # Connect to the compute instance currently running my sims:
 PROJECT="droc-199608"
 ZONE="us-east1-b"
-INSTANCE="psodroc-ce-3"
+INSTANCE="psodroc-ce-4"
 gcloud compute --project "$PROJECT" ssh --zone "$ZONE" "$INSTANCE"
