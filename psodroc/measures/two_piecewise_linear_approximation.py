12c1085b8d26acd6fb45c4139ec4c8daa32aa51c
phlippieb <phlippie.bosman@gmail.com>
2018-05-06 21:32:03 +0200

    Fix DRoC measure: fix start of two-piecewise linear approximation to first y-value

--- psodroc/measures/two_piecewise_linear_approximation.py
+++ psodroc/measures/two_piecewise_linear_approximation.py
@@ -1,65 +1,83 @@
 from scipy import optimize
 import numpy as np
-
-# A structure to hold the result of a two-piecewise linear approximation calculation.
-# A two-piecewise linear approximation consist of two straight lines that meet at a breakpoint.
-# Each line is an approximation of some data that yields the least error.
+        
+# A Two-piecewise linear approximation.
+# The approximation is represented in a format that fascilitates fixing the y-value at x=0.
 class TwoPiecewiseLinearApproximation:
-    # The x-coordinate of the breakpoint
-    x = 0
-
-    # The y-coordinate of the breakpoint
-    y = 0
-
-    # The slope of the first line, which ends at the breakpoint
+    # The y-coordinate at x=0
+    y0 = 0
+    
+    # The slope of the first line, which ends at the breakpoint:
     m1 = 0
-
-    # The slope of the second line, which starts at the breakpoint.
+    
+    # The x-coordinate at the breakpoint:
+    x1 = 0
+    
+    # The slope of the second line, which starts at the breakpoint:
     m2 = 0
-
-    def __init__(self, x, y, m1, m2):
-        self.x = x
-        self.y = y
+    
+    # Initialize the object's properties directly
+    def __init__(self, y0, m1, x1, m2):
+        self.y0 = y0
         self.m1 = m1
+        self.x1 = x1
         self.m2 = m2
 
 # Given x- and y-values of a set of data, fit a two-piecewise linear approximation to the data.
 def fit_to(xs, ys):
     # Ensure that xs are floats:
     xs = [float(x) for x in xs]
-
+    
     # Bound the parameters of _2pwl as follows:
-    # - x0 (the breakpoint x value) is between 0 and the max x
-    # - y0 (the breakpoint y value) is between 0 and the max y
-    # - m1 (the slope of the first line) is expected to be between 0 and -inf (though it might go above 0 in some cases, so we leave it unbounded)
-    # - Ditto for m2 (the slope of the second line)
-    bounds = ([0, 0, -np.inf, -np.inf], [max(xs), max(ys), np.inf, np.inf])
+    # - m1 (the slope of the first line) is expected to be between 0 and -inf
+    # - x2 (the breakpoint x value) is between 0 and the max x
+    # - m2 (the slope of the second line) is expected to be between 0 and -inf, but it could go above 0, and is left unbounded
+    bounds = ([-np.inf, 0, -np.inf], [0, max(xs), np.inf])
 
-    # Estimate that the breakpoint between the lines will be halfway down max(xs),
+    # Estimate that the breakpoint will be halway along the x-axis,
     # that the first line's slope will be -2 (slightly steeper than a 45 degree angle).
     # and that the second line's slope will be -0.5 (slightly shallower than a 45 degree angle).
-    p0 = [max(xs)/2, ys[int(max(xs))/2], -2, -0.5]
+    p0 = [-2, max(xs)/2, -0.5]
 
     # Optimize a two-piecewise linear approximation of the given data.
-    # optimal_params will be the [x0, y0, m1, m2] that yield the optimal linear approximation of the given data.
+    # optimal_params will be the [m1, x2, m2] that yield the optimal linear approximation of the given data.
     # error is ignored.
-    optimal_params, error = optimize.curve_fit(_2pwl, xs, ys, bounds=bounds, p0=p0)
+    # optimal_params, error = optimize.curve_fit(_2pwl, xs, ys, bounds=bounds, p0=p0)
+    y0 = ys[0]
+    q = get_2pwl(ys[0])
+    optimal_params, error = optimize.curve_fit(q, xs, ys, bounds=bounds, p0=p0)
 
     # Return the result as a TwoPiecewiseLinearApproximation object
-    return TwoPiecewiseLinearApproximation(optimal_params[0], optimal_params[1], optimal_params[2], optimal_params[3])
+    # return TwoPiecewiseLinearApproximation_old(optimal_params[0], optimal_params[1], optimal_params[2], optimal_params[3])
+    return TwoPiecewiseLinearApproximation(y0=y0, m1=optimal_params[0], x1=optimal_params[1], m2=optimal_params[2])
 
 # Plot a given two-piecewise linear function between 0 and the given maxX.
-def plot(maxX, tpwla):
-    xs = np.linspace(0, maxX, maxX)
-    import matplotlib.pyplot as plt
-    plt.plot(xs, _2pwl(xs, tpwla.x, tpwla.y, tpwla.m1, tpwla.m2), color="black")
+# TODO: fix this function to use new representation
+# def plot(maxX, tpwla):
+#     xs = np.linspace(0, maxX, maxX)
+#     import matplotlib.pyplot as plt
+#     plt.plot(xs, _2pwl(xs, tpwla.x, tpwla.y, tpwla.m1, tpwla.m2), color="black")
 
-# This function gives two lines with the given slopes that break at the given coordinates.
-# When fitting a piecewise approximation on some data, the error between the data and this function are minimized.
-# Also used to plot a piecewise approximation.
-# x0: the x-coordinate of the breakpoint between the two lines
-# y0: the y-coordinate of the breakpoint between the two lines
-# k1: the slope of the first line
-# k2: the slope of the second line
-def _2pwl(x, x0, y0, m1, m2):
-    return np.piecewise(x, condlist=[x < x0], funclist=[lambda x: m1*x + y0 - m1*x0, lambda x: m2*x + y0 - m2*x0])
+# This function returns a piecewise object.
+# y1: The y-value at x=0
+# m1: The slope of the first line
+# x2: The x-value at the breakpoint
+# m2: The slope of the second line
+def _2pwl(x, y0, m1, x2, m2):
+    return np.piecewise(x,
+        condlist=[x<x2],
+        funclist=[
+            lambda x: m1*x + y0,
+            lambda x: m2*(x + x2) + (m1*x2 + y0)
+        ])
+
+# Partially apply the given y0 to _2pwl in order to fix the starting value (at x=0) of the piecewise object to the given y-value.
+def get_2pwl(y0):
+    def _2pwl(x, m1, x2, m2):
+        return np.piecewise(x,
+            condlist=[x<x2],
+            funclist=[
+                lambda x: m1*x + y0,
+                lambda x: m2*(x + x2) + (m1*x2 + y0)
+            ])
+    return _2pwl
