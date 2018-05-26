aed0bdd437d1fad1eb71c102309b9b7064795b25
phlippieb <phlippie.bosman@gmail.com>
2018-05-26 15:55:54 +0200

    Update DM; use max for s in [.05, .15]

--- psodroc/measures/funnels.py
+++ psodroc/measures/funnels.py
@@ -1,49 +1,71 @@
 import numpy as np
 from scipy.spatial.distance import pdist as pairwise_distance
-from numba import jit
+import itertools
 
-# This metric is used to estimate the presence of multiple funnels. It returns a value in the range [-disp_D, sqrt(D) - disp_D],
-# where D is the number of dimensions, and disp_D, a (pre-calculatable) constant, is the dispersion of a large uniform sample in
-# D dimensions. A dispersion metric > 0 indicates the presence of multiple funnels.
-# Parameter function:   The fitness function to run the measure on.
-# Parameter domain_min: The min of the function domain. Assumed to be the same in all dimensions.
-# Parameter domain_max: The max of the function domain. Assumed to be the same in all dimensions.
-# Parameter dimensions: The number of dimensions the function is defined in.
-# Returns:              A scalar in [-disp_D, sqrt(D) - disp_D]. A positive return value indicates the presence of multiple funnels.
-def dispersion_metric(function, domain_min, domain_max, dimensions):
-    return _dispersion_metric(function, domain_min, domain_max, dimensions, 1000, 100)
-    
-def _dispersion_metric(function, domain_min, domain_max, dimensions, n, s):
-    sample_positions = np.random.uniform(domain_min, domain_max, (n, dimensions))
-    sample_fitnesses = np.array([function(sample_position) for sample_position in sample_positions])
-    
+
+def DM(function, domain_min, domain_max, dimensions):
+    # This metric is used to estimate the presence of multiple funnels. It returns a value in the range [-disp_D, sqrt(D) - disp_D],
+    # where D is the number of dimensions, and disp_D, a (pre-calculatable) constant, is the dispersion of a large uniform sample in
+    # D dimensions. A dispersion metric > 0 indicates the presence of multiple funnels.
+    # function: Callable. The fitness function to run the measure on.
+    # domain_min: Float-like. The min of the function domain. Assumed to be the same in all dimensions.
+    # domain_max: Float-like. The max of the function domain. Assumed to be the same in all dimensions.
+    # dimensions: Int. The number of dimensions the function is defined in.
+    # Returns: Float-like. A scalar in [-disp_D, sqrt(D) - disp_D]. A positive return value indicates the presence of multiple funnels.
+    max_DM = None
+    for s in np.arange(.05, .15, .001):
+        DM = _DM(function, domain_min, domain_max, dimensions, 100, s)
+        max_DM = DM if max_DM is None else max(max_DM, DM)
+    return max_DM
+
+
+def _DM(function, domain_min, domain_max, dimensions, n, s):
+    # function: Callable. The fitness function to run the measure on.
+    # domain_min: Float-like. The min of the function domain. Assumed to be the same in all dimensions.
+    # domain_max: Float-like. The max of the function domain. Assumed to be the same in all dimensions.
+    # dimensions: Int. The number of dimensions the function is defined in.
+    # n: Int. The size of the large sample.
+    # s: Float-like. The proportion of the large sample to use as the fittest sub-sample.
+    # Returns: Float-like. A scalar in [-disp_D, sqrt(D) - disp_D]. A positive return value indicates the presence of multiple funnels.
+    sample_positions = np.random.uniform(
+        domain_min, domain_max, (n, dimensions))
+
+    sample_fitnesses = np.array([function(sample_position)
+                                 for sample_position in sample_positions])
+
     # Get the indices of the s smallest fitnesses from the sample, and use it to get the corresponding positions.
-    s_indices = sample_fitnesses.argsort()[:s]
+    s_size = int(n * s)
+    s_indices = sample_fitnesses.argsort()[:s_size]
     s_positions = sample_positions[s_indices]
-    
-    # Normalise the positions to be in a domain of [0, 1]
+
+    # Normalize the positions to be in a domain of [0, 1]
     s_positions_normalized = _normalize(s_positions, domain_min, domain_max)
-    
+
     dispersion = _dispersion(s_positions_normalized)
     return dispersion - _dispersion_const(dimensions)
-    # return 0
+
 
 def _normalize(xs, x_min, x_max):
     return [(x - x_min) / (x_max - x_min) for x in xs]
-    
-# The dispersion of a set of positions is the average pair-wise distance between those positions.
-@jit # (Make things faster)
+
+
 def _dispersion(xs):
+    # The dispersion of a set of positions is the average pair-wise distance between those positions.
+
     # We need at least 2 positions to calculate pairwise distances
     if len(xs) < 2:
         return 0
-    
+
     pairwise_distances = pairwise_distance(xs, 'euclidean')
     return np.average(pairwise_distances)
-    
-# Returns a "pre-determined constant value for the dispersion of a large uniform random sample in a D-dimensional search space, normalised to [0, 1] in all dimensions"
-# We will not be pre-determining constant values for every possible D, so we will calculate it instead.
+
+
+_dispersion_consts = {}
+
+
 def _dispersion_const(D):
+    # Returns a "pre-determined constant value for the dispersion of a large uniform random sample in a D-dimensional search space, normalised to [0, 1] in all dimensions"
+    # We will not be pre-determining constant values for every possible D, so we will calculate it instead.
     if D in _dispersion_consts:
         return _dispersion_consts[D]
     else:
@@ -52,5 +74,3 @@ def _dispersion_const(D):
         # Calculate, store, and return the dispersion of such a sample
         _dispersion_consts[D] = _dispersion(xs)
         return _dispersion_consts[D]
-    
-_dispersion_consts = {}
--- run.py
+++ run.py
@@ -1,22 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import psodroc.measures.ruggedness as ruggedness
+import psodroc.measures.funnels as funnels
 import psodroc.benchmarks.spherical as benchmark
 import numpy as np
 dimensionality = 1
 
-f_01 = []
+a = []
 for i in range(30):
-    r_01 = ruggedness.FEM_0_01(
+    b = funnels.DM(
         benchmark.function, benchmark.min(0), benchmark.max(0), dimensionality)
-    f_01.append(r_01)
-a_01 = np.mean(f_01)
-print '0.01:\t', a_01
-
-f_1 = []
-for i in range(30):
-    r_1 = ruggedness.FEM_0_1(
-        benchmark.function, benchmark.min(0), benchmark.max(0), dimensionality)
-    f_1.append(r_1)
-a_1 = np.mean(f_1)
-print '0.1:\t', a_1
+    a.append(b)
+c = np.mean(a)
+print 'Result:\t', c
