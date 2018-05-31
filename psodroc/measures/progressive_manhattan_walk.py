a2d806210dc31e3a2fd4cc9ced0894cc33bb1e8f
phlippieb <phlippie.bosman@gmail.com>
2018-05-31 14:48:36 +0200

    Create progressive manhattan walk

--- psodroc/measures/progressive_manhattan_walk.py
+++ psodroc/measures/progressive_manhattan_walk.py
@@ -8,13 +8,13 @@ def single_walk(n, domain_min, domain_max, num_steps, s, starting_zone):
     # domain_min: Int. The domain minimum of the problem. The same domain min is used in each dimension.
     # domain_max: Int. The domain maximum of the problem. The same domain min is used in each dimension.
     # num_steps: Int. The number of steps in the walk.
-    # s: Float-like. The step size upper bound.
+    # s: Float-like. The step size. Fixed.
     # starting_zone: [Int]. A binary array of size n. Specifies the starting zone of the walk. The starting zone is a hypercube in one corner of the problem space. Each bit in the array specifies, for each corresponding dimension, whether the starting zone is in the lower or upper half of that dimension's domain; a 0 indicates that the walk should start in [domain_min, domain_range/2] in that dimension, and a 1 indicates that it should start in [domain_range/2, domain_max].
 
     # An array of num_steps n-dimensional vectors for storing results
     walk = np.zeros([num_steps, n])
 
-    # Keep track of the middle of the domain.
+    # The mid-point of the domain.
     domain_mid = (domain_max - domain_min) / 2.
 
     # Set the initial position of the walk.
@@ -35,31 +35,24 @@ def single_walk(n, domain_min, domain_max, num_steps, s, starting_zone):
 
     # Add steps to the walk.
     for step in range(1, num_steps):
+        # Select a random dimension.
+        rD = np.random.randint(n)
+
+        # Update each dimension component.
         for i in range(n):
-            # Get a random step size in [0, s) in the direction away from the starting zone.
-            direction = 1. if starting_zone[i] == 0 else -1.
-            r = np.random.uniform(low=0, high=s) * direction
+            if i == rD:
+                # Take a fixed-size step away from the starting zone in the randomly-selected dimension.
+                direction = +1. if starting_zone[i] == 1 else -1.
+                walk[step][i] = walk[step-1][i] + (direction * s)
 
-            # Add the step size to the previous step to get the new step.
-            walk[step][i] = walk[step-1][i] + r
+                # If the boundary was overstepped, mirror the step and flip the corresponding starting zone bit.
+                if walk[step][i] < domain_min or domain_max < walk[step][i]:
+                    walk[step][i] = walk[step-1][i] - (direction * s)
+                    starting_zone[i] = 1 if starting_zone[i] == 0 else 0
 
-            # If the step is outside the problem domain, handle.
-            if walk[step][i] < domain_min or domain_max < walk[step][i]:
-                # If the minimum boundary was overstepped, *this* value will be negative:
-                e1 = walk[step][i] - domain_min
-                # Else, *this* value will be negative:
-                e2 = domain_max - walk[step][i]
-
-                # Determine the amount that the boundary was overstepped with as `e`,
-                # as well as the index the boundary that was overstepped (min or max) as `ei`.`
-                e, ei = (np.min([e1, e2]), np.argmin([e1, e2]))
-
-                # Mirror the position inside the domain.
-                mirrored_step = 2 * e * (1 if ei == 1 else -1)
-                walk[step][i] += mirrored_step
-
-                # Flip the corresponding bit in the starting zone to guide the walk in the opposite direction.
-                starting_zone[i] = 1 if starting_zone[i] == 0 else 0
+            else:
+                # Do not update position in non-selected dimensions.
+                walk[step][i] = walk[step-1][i]
 
     # All steps are complete. Return the walk.
     return walk
@@ -72,7 +65,7 @@ def multiple_walks(n, domain_min, domain_max, s):
     # Out of 2^n starting zones, only every (2^n)/n-th zone is used.
     # n: Int. The number of dimensions of the problem space.
     # domain_min, domain_max: Float-like. The domain of the problem space.
-    # s: Float-like. The max step size as a proportion of the domain range; e.g. 0.1 for a 10-th of the domain range.
+    # s: Float-like. The fixed step size as a proportion of the domain range; e.g. 0.1 for a 10-th of the domain range.
     increment = (2 ** n) / n
     starting_zones = np.zeros([n, n], dtype=int)
     for z in range(n):
--- run.py
+++ run.py
@@ -1,43 +1,8 @@
-# -*- coding: utf-8 -*-
+import psodroc.measures.progressive_manhattan_walk as walk
+import matplotlib.pyplot as plt
 
-import numpy as np
+for walk in walk.multiple_walks(2, -1, 1, 0.1):
+    xs, ys = (walk.T[0], walk.T[1])
+    plt.plot(xs, ys, linestyle='--')
 
-import psodroc.measures.neutrality as neutrality
-
-import psodroc.benchmarks.step as step
-import psodroc.benchmarks.spherical as spherical
-
-# Dimensionality:
-d = 5
-
-# Spherical is not neutral at all:
-print 'spherical:'
-print '- PN =', neutrality.PN(
-    spherical.function, spherical.min(0), spherical.max(0), d)
-print '- LSN =', neutrality.LSN(
-    spherical.function, spherical.min(0), spherical.max(0), d)
-
-# Step contains neutrality, but it is not very connected.
-print 'step:'
-print '- PN =', neutrality.PN(step.function, step.min(0), step.max(0), d)
-print '- LSN =', neutrality.LSN(step.function, step.min(0), step.max(0), d)
-
-
-def neutral(xs):
-    # A purely neutral function:
-    return 0.
-
-
-print 'neutral:'
-print '- PN =', neutrality.PN(neutral, -10, 10, d)
-print '- LSN =', neutrality.LSN(neutral, -10, 10, d)
-
-
-def step_like(xs):
-    # A step-like function with large steps:
-    return np.ceil(xs[0])
-
-
-print 'step-like:'
-print '- PN =', neutrality.PN(step_like, -10, 10, d)
-print '- LSN =', neutrality.LSN(step_like, -10, 10, d)
+plt.show()
