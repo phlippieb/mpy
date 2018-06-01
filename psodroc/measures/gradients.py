Fix grad measures normalisation (I think) and improve performance

psodroc/measures/gradients.py
@@ -3,10 +3,17 @@ import numpy as np
 
 
 def G_measures(function, domain_min, domain_max, dimensions):
-    s = 0.001
     starting_zones = manhattan.get_starting_zones(dimensions)
-    num_steps = manhattan.get_num_steps(dimensions, s)
-    step_size = manhattan.get_step_size(domain_min, domain_max, s)
+
+    # Approach 1:
+    # s = 0.001
+    # num_steps = manhattan.get_num_steps(dimensions, s)
+    # step_size = manhattan.get_step_size(domain_min, domain_max, s)
+
+    # Approach 2:
+    num_steps = 1000
+    step_size = ((domain_max - domain_min) * dimensions) / 1000
+
     walks = [manhattan.walk(dimensions, domain_min, domain_max, num_steps,
                             step_size, starting_zone) for starting_zone in starting_zones]
 
@@ -14,45 +21,49 @@ def G_measures(function, domain_min, domain_max, dimensions):
     #       - fix and update my thesis.
 
     # Determine the gradient between each consecutive step.
-    gradients = _gradients2(function, domain_min,
-                            domain_max, dimensions, walks, step_size)
+    gradients = _gradients(function, domain_min,
+                           domain_max, dimensions, walks, step_size)
+
     # Use the absolute value of each gradient;
     # this is because we don't care about the direction of the gradients,
     # and don't want "ups" and "downs" to cancel out.
     abs_gradients = np.absolute(gradients)
 
     # Simply determine and return the average and standard deviation of the absolute gradients.
-    return np.average(abs_gradients), np.std(abs_gradients)
+    avg = np.average(abs_gradients)
+    dev = np.std(abs_gradients)
+    return avg, dev
 
 
-def _gradients2(function, domain_min, domain_max, dimensions, walks, step_size):
+def _gradients(function, domain_min, domain_max, dimensions, walks, step_size):
     gradients = []
+    f_range = _f_range(function, walks)
+    x_range = domain_max - domain_min
 
     for walk in walks:
-
-        fitnesses = [function(xs) for xs in walk]
-
-        # The gradient between each steps is normalised.
-
-        # Obtain the min and max fitness.
-        fitness_range = np.max(fitnesses) - np.min(fitnesses)
-
-        # Obtain the total Manhattan distance between the position vectors defining the bounds of the search space.
-        bounds_range = np.sum(
-            [domain_max - domain_min for i in range(dimensions)])
-        denominator = step_size / bounds_range
-
-        f1s = fitnesses[:-1]
-        f2s = fitnesses[1:]
-
-        walk_gradients = [_gradient2(f1, f2, np.max(fitnesses), np.min(
-            fitnesses), step_size, domain_max, domain_min, dimensions) for f1, f2 in zip(f1s, f2s)]
-        gradients += walk_gradients
+        fs = [function(xs) for xs in walk]
+        f1s = fs[:-1]
+        f2s = fs[1:]
+        gradients = [_gradient(
+            f1, f2, f_range, step_size, x_range, dimensions)
+            for f1, f2 in zip(f1s, f2s)]
+        # gradients += walk_gradients
 
     return gradients
 
 
-def _gradient2(f1, f2, fmax, fmin, s, xmax, xmin, n):
-    numerator = (f2 - f1) / (fmax - fmin)
-    denominator = s / (1 * (xmax - xmin))
-    return numerator / denominator
+def _f_range(function, walks):
+    f_min = None
+    f_max = None
+    for walk in walks:
+        for xs in walk:
+            f = function(xs)
+            f_min = f if f_min is None else min(f_min, f)
+            f_max = f if f_max is None else max(f_max, f)
+    return f_max - f_min
+
+
+def _gradient(f1, f2, f_range, s, x_range, n):
+    numer = (f2 - f1) / f_range
+    denom = s / (n * x_range)
+    return numer / denom