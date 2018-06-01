cfe408a5abf51ec07162f0b3e0ad90319cf5b6d9
phlippieb <phlippie.bosman@gmail.com>
2018-06-01 21:13:54 +0200

    Update ruggedness measures: find stability point quicker; only compute entropy for 20 e values

--- psodroc/measures/ruggedness.py
+++ psodroc/measures/ruggedness.py
@@ -1,6 +1,6 @@
 from __future__ import division  # make division cast to double by default
 import math
-import progressive_random_walk as walk
+import progressive_random_walk as random_walk
 import itertools
 import numpy as np
 
@@ -25,85 +25,157 @@ def FEM_0_01(function, domain_min, domain_max, dimensions):
 
 
 def _FEM(function, domain_min, domain_max, dimensions, max_step_size_fraction):
-    starting_zones = walk.get_starting_zones(dimensions)
+    starting_zones = random_walk.get_starting_zones(dimensions)
+
+    # TEST: quicker; just one walk
+    # starting_zones = starting_zones[:1]
 
     # Approach 1:
-    s = 0.02
-    num_steps = walk.get_num_steps(dimensions, s)
-    step_size = walk.get_step_size(domain_min, domain_max, s)
+    # num_steps = walk.get_num_steps(dimensions, max_step_size_fraction)
+    # step_size = walk.get_step_size(
+    #     domain_min, domain_max, max_step_size_fraction)
 
-    walks = [walk.walk(dimensions, domain_min, domain_max, num_steps,
-                       step_size, starting_zone) for starting_zone in starting_zones]
+    # Approach 2 (Malan's thesis):
+    num_steps = 1000
+    step_size = random_walk.get_step_size(
+        domain_min, domain_max, max_step_size_fraction)
+
+    walks = [random_walk.walk(dimensions, domain_min, domain_max, num_steps,
+                              step_size, starting_zone) for starting_zone in starting_zones]
 
     max_entropy = 0
 
     # Handle each walk individually.
-    for xs in walks:
-        walk_fitnesses = [function(x) for x in xs]
-        # Set the entropy estimation sensitivity to 0 to begin with:
-        E = 0
+    for (i, walk) in enumerate(walks):
+        fs = [function(xs) for xs in walk]
 
-        # Increment E by 0.05 until the resulting entropy string is flat.
-        # Calculate the entropy of the string for each E.
-        while True:
-            entropy_string = _entropy_string(walk_fitnesses, E)
-            if _is_entropy_string_flat(entropy_string):
-                break
-            entropy = _entropy(entropy_string)
-            # If this entropy is greater than the previous max, store it.
-            # is_max = '*' if entropy > max_entropy else ''
+        # Find the stability measure (E_star).
+        E_star = find_stability2(fs)
 
-            # if entropy > max_entropy:
-            #     print entropy_string
+        for E in np.arange(0, E_star, .05 * E_star):
+            string = _string(fs, E)
 
+            # Update the max entropy so far.
+            entropy = _entropy(string)
             max_entropy = max(entropy, max_entropy)
-            E += .05
 
-            # print E, '\t', entropy, '\t', is_max
+            E += (.05 * E_star)
 
     return max_entropy
 
 
-def _entropy_string(fitnesses, E):
-    # Build a string of symbols indicating pair-wise changes in the given array of fitnesses.
-    # A difference between a pair of fitnesses that is less than the sensitivity parameter E is represented by a 0.
-    # A difference larger than E is represented by -1 or +1, depending on which fitness in the pair is larger.
-    string = []
-    fitness_pairs = [(fitnesses[i], fitnesses[i+1])
-                     for i in range(0, len(fitnesses) - 1)]
-    for f0, f1 in fitness_pairs:
-        if abs(f0 - f1) <= E:
-            string.append(0)
-        elif f0 - f1 < -E:
-            string.append(-1)
-        elif f0 - f1 > E:
-            string.append(1)
-    return string
+def find_stability2(fs):
+    return find_smallest(lambda E: _is_stable(fs, E), -3)
+
+
+def find_stability(fs, min_increment):
+    # Find a lower and upper bound for the stability point on an exponential scale.
+    lower = upper = min_increment
+    while True:
+        if _is_stable(fs, upper):
+            break
+
+        lower = upper
+        upper += 1
+
+    # upper is now greater than the stability measure, and lower is smaller.
+
+    delta = upper - lower
+    while delta >= min_increment:
+        mid = (lower + upper) / 2.
+
+        if _is_stable(fs, mid):
+            # The mid point between lower and upper is above (or equal to) the stability point.
+            # Search the lower half of the range.
+            upper = mid
+        else:
+            # The mid point between lower and upper is below (or equal to) the stability point.
+            # Search the upper half of the range.
+            lower = mid
+
+        delta = upper - lower
+
+    return upper
+
+
+def _is_stable(fs, E):
+    for i in range(len(fs)-1):
+        if abs(fs[i] - fs[i+1]) > E:
+            return False
+    return True
+
+
+def _is_stable2(fs, E):
+    return
+
+
+def _string(fs, E):
+    string = [_symbol(f1, f2, E) for f1, f2 in zip(fs[:-1], fs[1:])]
+    # is_flat = np.count_nonzero(string) == 0
+    return string  # , is_flat
+
+
+def _symbol(f1, f2, E):
+    if abs(f1 - f2) <= E:
+        return 0
+    elif f1 - f2 < -E:
+        return -1
+    else:
+        return 1
+
+
+_pq_permutations = [(-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0)]
 
 
 def _entropy(entropy_string):
     # Compute the probability of the substring [p, q] in entropy_string for each p and q in [-1, 0, 1] such that p != q.
     # (This gives the probability of each non-flat symbol.)
-    permutations = itertools.permutations([-1, 0, 1], 2)
-    Ps = [_probability(p, q, entropy_string) for p, q in permutations]
+    # permutations = itertools.permutations([-1, 0, 1], 2)
+    global _pq_permutations
+    Ps = [_probability(p, q, entropy_string) for p, q in _pq_permutations]
     # Return the entropy based on those probabilities:
-    # TODO: the 0 if P == 0 part is needed because log breaks otherwise, but it is not specified in the log formula. Check it out.
     return -sum(0 if P == 0 else (P * math.log(P, 6)) for P in Ps)
 
 
-# Returns the number of occurrences of pq in the given string, divided by the length of the string.
 def _probability(p, q, entropy_string):
+    # Returns the number of occurrences of pq in the given string, divided by the length of the string.
+
     # Count the number of times p and q occur consecutively in the string:
     occurrences = sum(1 if p_ == p and q_ == q else 0 for p_,
                       q_ in zip(entropy_string[:-1], entropy_string[1:]))
+
     # The probability of pq is the number of occurrences out of the length of the string.
     return occurrences / len(entropy_string)
 
-# True if and only if the string contains only 0s, indicating no significant differences in fitnesses in the walk.
 
+# UTIL
 
-def _is_entropy_string_flat(entropy_string):
-    if 1 in entropy_string or -1 in entropy_string:
-        return False
-    else:
-        return True
+def find_smallest(condition, min_e):
+    # Find and return the smallest value that meets the given condition.
+    # The result is accurate up to the exponential precision specified by min_e;
+    # eg if min_e=-3, the precision is 0.001.
+
+    # Find the smallest precision that overshoots the condition.
+    e = 0
+    while True:
+        if condition(10 ** e):
+            break
+        e += 1
+
+    # Keep track of a valid range:
+    lower = 0.
+    upper = 10. ** e
+
+    # Search within the valid range at incrementally increasing precision.
+    while e > min_e:
+        e -= 1
+        inc = 10. ** e
+
+        for t in np.arange(lower, upper, inc):
+            if condition(t):
+                upper = t
+                break
+            else:
+                lower = t
+
+    return upper
--- run.py
+++ run.py
@@ -1,39 +1,35 @@
-import psodroc.measures.gradients as gradients
+import psodroc.measures.ruggedness as ruggedness
 import numpy as np
 from termcolor import colored
 
-import psodroc.benchmarks.ackley as benchmark
+import psodroc.benchmarks.griewank as benchmark
 f = benchmark.function
 f_min = benchmark.min(0)
 f_max = benchmark.max(0)
-ds = [1, 2, 5, 15, 30]
+# ds = [1, 2, 5, 15, 30]
+ds = [1000]
 
 for d in ds:
-    print colored('{} dimensions:'.format(d), 'blue')
-    g_avgs = []
-    g_devs = []
+    print colored('{}D'.format(d), 'blue')
+    fem1s = []
+    fem01s = []
     samples = 30
     for i in range(samples):
         print colored('running {} of {} samples...'.format(
             i+1, samples), 'white'), '\r',
-        g_avg, g_dev = gradients.G_measures(f, f_min, f_max, d)
-        g_avgs.append(g_avg)
-        g_devs.append(g_dev)
-    g_avg = np.average(g_avgs)
-    g_dev = np.average(g_devs)
+        fem01 = ruggedness.FEM_0_01(f, f_min, f_max, d)
+        fem01s.append(fem01)
     print '                                             \r',
-    print 'avg:', g_avg, '\ndev:', g_dev
+    print 'FEM 0.01:', np.average(fem01s)
+
+    for i in range(samples):
+        print colored('running {} of {} samples...'.format(
+            i+1, samples), 'white'), '\r',
+        fem1 = ruggedness.FEM_0_1(f, f_min, f_max, d)
+        fem1s.append(fem1)
+    print '                                             \r',
+    print 'FEM 0.1: ',  np.average(fem1s)
+
     print ''
+
 print colored('done.', 'green')
-
-"""
-Expected values (Ackley):
-
-D   | avg | dev
---- | --- | ---
-1   | 13  | 8
-2   | 33  | 22
-5   | 35  | 20
-15  | 3.5 | 2.9
-30  | 3.8 | 3.1
-"""
