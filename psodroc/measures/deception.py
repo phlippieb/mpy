7c9afab48d6ae7b39641ca99dda7c8ba8a3bd201
phlippieb <phlippie.bosman@gmail.com>
2018-06-01 22:22:46 +0200

    Add FDC for deception

---
+++ psodroc/measures/deception.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.spatial import distance
+
+
+def FDC(function, domain_min, domain_max, dimensions):
+    # TODO: which sample_size to use?
+
+    # Sample some positions and determine their fitnesses.
+    sample_size = 5
+    xs = np.random.uniform(low=domain_min, high=domain_max,
+                           size=[sample_size, dimensions])
+    fs = [function(x) for x in xs]
+
+    # Determine the fittest position from the sample.
+    x_star = xs[np.argmin(fs)]
+
+    # Determine the distance from each position to the fittest position.
+    ds = [distance.euclidean(x_star, x) for x in xs]
+
+    fm = np.mean(fs)
+    dm = np.mean(ds)
+
+    i_s = range(sample_size)
+    numer = np.sum([(fs[i] - fm) * (ds[i] - dm) for i in i_s])
+    denom = np.sqrt(np.sum([np.square(fs[i] - fm) for i in i_s])) * \
+        np.sqrt(np.sum([np.square(ds[i] - dm) for i in i_s]))
+    return numer / denom
