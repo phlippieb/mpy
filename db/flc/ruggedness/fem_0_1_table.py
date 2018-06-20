1a1878f916f442caedf2f7529a3d8d38ae80ae9b
phlippieb <phlippie.bosman@gmail.com>
2018-06-20 11:46:13 +0200

    Create ruggedness result services

--- db/flc/ruggedness/fem_0_1_table.py
+++ db/flc/ruggedness/fem_0_1_table.py
@@ -1,10 +1,10 @@
-import db as db
+from db import db
 import warnings
 
 
 def store(benchmark_name, dimensionality, experiment, measurement):
     db.cursor.execute(
-        'INSERT INTO FEM_0_01 (benchmark_name, dimensionality, experiment, measurement)' +
+        'INSERT INTO FEM_0_1 (benchmark_name, dimensionality, experiment, measurement)' +
         'VALUES (%s, %s, %s, %s)' +
         'ON CONFLICT (benchmark_name, dimensionality, experiment)' +
         'DO UPDATE SET measurement = %s',
@@ -17,13 +17,13 @@ def commit():
 
 def fetch(benchmark_name, dimensionality, experiment):
     db.cursor.execute(
-        'SELECT measurement FROM FEM_0_01 WHERE benchmark_name=%s AND dimensionality=%s AND experiment=%s',
+        'SELECT measurement FROM FEM_0_1 WHERE benchmark_name=%s AND dimensionality=%s AND experiment=%s',
         (benchmark_name, dimensionality, experiment))
     rows = db.cursor.fetchall()
     if len(rows) < 1:
         return None
     elif len(rows) > 1:
-        warnings.warn("Multiple results found when fetching FEM_0_01 with benchmark_name={} and dimensionality={} and experiment={}".format(
+        warnings.warn("Multiple results found when fetching FEM_0_1 with benchmark_name={} and dimensionality={} and experiment={}".format(
             benchmark_name, dimensionality, experiment))
 
     # rows is an array with an element for each returned row. We only expect one row (see warning above), so we take the first one.
--- run.py
+++ run.py
@@ -1,17 +1,4 @@
-from termcolor import colored
-import psodroc.measures.searchability as searchability
+from results.flc.ruggedness import fem_0_01
 
-import psodroc.benchmarks.rastrigin as benchmark
-f = benchmark.function
-f_min = benchmark.min(0)
-f_max = benchmark.max(0)
-ds = [1, 2, 5, 15, 30]
-# ds = [5]
-
-for d in ds:
-    print colored('{}D'.format(d), 'blue')
-    fci_soc, fci_cog, fci_sig = searchability.FCIs(f, f_min, f_max, d)
-    print '{:.3f}'.format(fci_cog), '{:.3f}'.format(fci_soc)
-    print ''
-
-print colored('done.', 'green')
+fem = fem_0_01.get('ackley', 1, 0, verbose=True)
+print fem
