468e036752d692a9ca11193c2068789669e44ef2
phlippieb <phlippie.bosman@gmail.com>
2018-06-22 22:58:06 +0200

    Revert "Store walks in the db -- slower than walking"
    This reverts commit 42d83a1a1d281904da1c6898a7955d4232576d63.

--- util/norm.py
+++
@@ -1,7 +0,0 @@
-def norm(xs, min_in=0., max_in=1., min_out=0., max_out=1.):
-    """Normalise xs from the input range to the output range."""
-    range_in = max_in - min_in
-    range_out = max_out - min_out
-    a = (max_out - min_out) / (max_in - min_in)
-    b = max_out - (a * max_in)
-    return [a * x + b for x in xs]
