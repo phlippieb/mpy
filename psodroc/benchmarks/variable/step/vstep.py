ae26412fcbe3e63afb3ed1c2e8f1d9ff04bbf1cc
phlippieb <phlippie.bosman@gmail.com>
2018-07-15 12:25:07 +0200

    Add variable step benchmark functions

---
+++ psodroc/benchmarks/variable/step/vstep.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def vstep(xs, step_size):
+    """Step function with variable step size.
+    step_size defines the minimum distance to the next step.
+    low = -20.
+    high = 20.
+    """
+    return np.sum(np.square(np.floor(x/step_size + .5)*step_size) for x in xs)
+
+# domain is [-20, 20] across all dimensions
+
+
+def min(d):
+    return -20.
+
+
+def max(d):
+    return 20.
+
+
+def is_dimensionality_valid(D):
+    return True
---
+++ psodroc/benchmarks/variable/step/vstep0_05.py
@@ -0,0 +1,17 @@
+import vstep
+
+
+def function(xs):
+    return vstep(xs, .05)
+
+
+def min(d):
+    return vstep.min(d)
+
+
+def max(d):
+    return vstep.max(d)
+
+
+def is_dimensionality_valid(D):
+    return vstep.is_dimensionality_valid(D)
